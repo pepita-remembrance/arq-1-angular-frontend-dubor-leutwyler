970d25824eab4264e179f20a7128d7fe77accde7
Ramshell <ramshellcinox@gmail.com>
2017-10-01 19:43:55 -0300

    Add angular app

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+app.use(bodyParser.json());
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+app.use(express.static(__dirname + '/dist'));
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
