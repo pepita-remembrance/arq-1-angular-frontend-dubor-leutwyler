c6604524994094c4c13643ab44b383653c71d685
Ramshell <ramshellcinox@gmail.com>
2017-12-22 11:03:23 -0300

    add refresh pages functionality

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var path = require('path');
 var app = express();
 app.use(bodyParser.json());
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
